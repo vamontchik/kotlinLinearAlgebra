15dec5a36e04c0c1d0343a55bca82d718279e326
Vladimir Montchik <vam4@illinois.edu>
2020-01-31 10:59:48 -0600

    remove commented out functionality and tests

--- test/LinAlgTest.kt
+++ test/LinAlgTest.kt
@@ -275,52 +275,6 @@ class LinAlgTest {
         vectorEquals(scalarDivide(scalar, base), result)
     }
 
-//    @Test
-//    fun simpleEigenTest() {
-//        val m: Matrix = arrayOf(
-//            arrayOf(2456.0, 1234.0),
-//            arrayOf(111.0, 987.0)
-//        )
-//        val eigenvaluesResult: Vector = arrayOf(
-//            2543.97, 899.026
-//        )
-//        val eigenvectorsResult: Matrix = arrayOf(
-//            arrayOf(14.0268, 1.0),
-//            arrayOf(-0.792563, 1.0)
-//        )
-//        val (eigenvalues, eigenvectors) = eigen(m)
-//
-//        println("eigenvalues found: ${eigenvalues.contentDeepToString()}")
-//        printMatrix("eigenvectors", eigenvectors)
-//
-//        vectorEquals(eigenvalues, eigenvaluesResult, 0.01)
-//        isScalarMultipleOfEachVector(eigenvectors, eigenvectorsResult)
-//    }
-//
-//    @Test
-//    fun simpleEigenTestTwo() {
-//        val m: Matrix = arrayOf(
-//            arrayOf(35.0, 45.0, 55.0),
-//            arrayOf(75.0, 83.0, 95.0),
-//            arrayOf(105.0, 115.0, 127.0)
-//        )
-//        val eigenvectorsResult: Matrix = arrayOf(
-//            arrayOf(0.402557, 0.733662, 1.0),
-//            arrayOf(-1.43706, 0.140923, 1.0),
-//            arrayOf(0.960684, -1.98979, 1.0)
-//        )
-//        val eigenvaluesResult: Vector = arrayOf(
-//            253.64, 7.68538, 0.954181
-//        )
-//        val (eigenvalues, eigenvectors) = eigen(m)
-//
-//        println("eigenvalues found: ${eigenvalues.contentDeepToString()}")
-//        printMatrix("eigenvectors", eigenvectors)
-//
-//        vectorEquals(eigenvalues, eigenvaluesResult, 0.01)
-//        isScalarMultipleOfEachVector(eigenvectors, eigenvectorsResult)
-//    }
-
     @Test
     fun identityTest() {
         val answer: Matrix = arrayOf(
